3a8f075a0324cc6ef31f8a5b814e7290ab24560b
katya <katyash-98@yandex.ru>
2024-02-14 20:14:43 +0300

    init2

--- DZZ/Main.java
+++ DZZ/Main.java
@@ -1,6 +1,7 @@
 package org.example.DZjava.DZZ;
 
-public class Main {    public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
     Phonebook phoneBook = new Phonebook();
     Display Menu = new Display(phoneBook);
     Menu.displayMenu();
